8a61afd70728098a630e717a1b384c633d0e98f2
INNOCV\david.ortiz <david.ortiz@innocv.com>
2018-01-13 13:25:41 +0100

    Demo

---
+++ demo.ts
@@ -0,0 +1,10 @@
+let variable: string ;
+let array: Array<any> = new Array<any>();
+
+variable = "This is a demo";
+array.push(1);
+array.push("hola");
+array.push(3);
+console.log(variable);
+array.map((row)=>console.log(row))
+
